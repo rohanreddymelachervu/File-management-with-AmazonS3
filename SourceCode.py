5be37a11c6ea3aca21275e0a4a6aa4ee8b4e5a6b
Rohan Reddy Melachervu <81566132+rohanreddymelachervu@users.noreply.github.com>
2021-10-15 16:27:24 +0530

    Update SourceCode.py

--- SourceCode.py
+++ SourceCode.py
@@ -7,8 +7,8 @@ from PIL import Image
 import os
 
 global s3
-s3 = boto3.client('s3', aws_access_key_id='your access key',
-                  aws_secret_access_key='your secret access key')
+s3 = boto3.client('s3', aws_access_key_id='<<your access key>>',
+                  aws_secret_access_key='<<your secret access key>>')
 
 
 def percent(part, total):
